0e9da9d1a493c82f944dc873d2e50972dd4fb000
MiloszPradela <milosz.pradela1@gmail.com>
2025-09-04 01:40:41 +0200

    Add project structure and config files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LostPassword from './pages/LostPassword';
+import ResetPassword from './pages/ResetPassword';
+import Home from './pages/Home';
+import MainLayout from './components/MainLayout';
+import ProtectedRoute from './components/ProtectedRoute';
+import AccountManagement from './pages/AccountManagement';
+import ManageSites from './pages/ManageSites';
+import CreatePost from './pages/CreatePost';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/lost-password" element={<LostPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        
+        <Route element={<ProtectedRoute />}>
+          <Route path="/"  element={<MainLayout><Home /></MainLayout>} />
+          <Route path="/account"  element={<MainLayout><AccountManagement /></MainLayout>} />
+          <Route path="/manage-sites" element={<MainLayout><ManageSites /></MainLayout>}  />
+          <Route path="/create-post" element={<MainLayout><CreatePost /></MainLayout>}  />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
